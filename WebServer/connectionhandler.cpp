48528eccbed95d5b135371226c7ce15c94c19b78
Semen <semen.cher@mail.ru>
2017-11-06 23:21:18 +0700

    Загружаю веб страничку, но она продолжает грузиться.

--- WebServer/connectionhandler.cpp
+++ WebServer/connectionhandler.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include "connectionhandler.h"
 
@@ -62,7 +63,43 @@ void ConnectionHandler::handlerThread()
 					int read = recv(socket_, buffer, IN_BUFFER_SIZE, 0);
 					if (read > 0) {
 						std::string str(buffer);
+						// TODO: Убрать эту строчку когда закончу.
 						std::cout << "\n" << str << "\n";
+
+						std::string page;
+						std::fstream file;
+						file.open("index.html", std::ios::in | std::ios::binary);
+
+						file.seekg(0, std::ios::end);
+						size_t length = static_cast<size_t>(file.tellg());
+						file.seekg(0, std::ios::beg);
+
+						while (length > IN_BUFFER_SIZE) {
+							file.read(buffer, IN_BUFFER_SIZE);
+							page += std::string(buffer);
+							length -= IN_BUFFER_SIZE;
+						}
+						if (length > 0) {
+							file.read(buffer, length);
+							buffer[length] = '\0';
+							page += std::string(buffer);
+						}
+
+						std::string answer = "HTTP/1.1 200 OK\n";
+						answer += "Server: localhost:21345\n";
+						answer += "Content - Language : ru\n";
+						answer += "Content - Type : text / html; charset = utf - 8\n";
+						answer += "Content - Length: " + std::to_string(page.size()) + "\n";
+						answer += "Connection : close \n\n";
+						answer += page;
+
+						// Отправляем ответ.
+						int result = send(socket_, answer.c_str(), answer.size(), 0);
+						if (result == SOCKET_ERROR)
+						{
+							printf("send from client failed with error: %d\n", WSAGetLastError());
+							break;
+						}
 					}
 				}
 			}
